b2bfcfd5be2db2f96ce1543ffb674b940fcedccb
Pedro Ramos <pedrowarkenramos@gmail.com>
2024-05-22 13:12:31 -0300

    feat: attack animation

--- Assets/Scripts/TransformExtensions.cs
+++ Assets/Scripts/TransformExtensions.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public static class TransformExtensions
 {
-    public static int GetSortingOrder(this Transform transform, float offsetY = 0)
+    public static int GetSortingOrder(this Transform transform, float offset = 0)
     {
-        return -(int)((transform.position.y + offsetY) * 100);
+        return Mathf.RoundToInt((transform.position.y + offset) * -100);
     }
 }
